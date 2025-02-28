006582dd10b0e29c2733776327fcd3cc3f2299e5
sourabh <sourabhyadav1302@gmail.com>
2025-02-28 14:41:30 +0530

    creating test role

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
-
-output "iam_role_arn" {
-  description = "The ARN of the IAM Role"
-  value       = aws_iam_role.custom_role.arn
-}
-
-output "iam_policy_arn" {
-  description = "The ARN of the IAM Policy"
-  value       = aws_iam_policy.custom_policy.arn
-}
+#
+# output "iam_role_arn" {
+#   description = "The ARN of the IAM Role"
+#   value       = aws_iam_role.custom_role.arn
+# }
+#
+# output "iam_policy_arn" {
+#   description = "The ARN of the IAM Policy"
+#   value       = aws_iam_policy.custom_policy.arn
+# }
 
